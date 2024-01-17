e399a397a666af8639df4eb8160e7ad011ae3a55
sosullivan7221 <sosullivan7221@gmail.com>
2024-01-17 00:56:03 -0500

    slight app.py changes

--- app.py
+++ app.py
@@ -2,36 +2,39 @@ from flask import Flask, render_template
 import pandas as pd
 from flask import request
 import random
+import logging
 
 
 app = Flask(__name__)
 
-
-
 @app.route('/', methods = ['GET', 'POST'])
 def home():
-    data = pd.read_csv('Pokedex_Ver_SV2.csv')
-    # if the request is a POST request then we will get the data from the form
+    poke_data = pd.read_csv('Pokedex_Ver_SV2.csv')
+    
     if request.method == 'POST':
         dataForm = request.form
-        print('Data from form: ', dataForm)
-        ## if data from form is a empty dictionary, lets select a random number betweeen 1 and 1000
-        if dataForm == {}:
-            dataForm = {'number': random.randint(1,1010)}
-            print('Random number selected: ', dataForm['number'])
-        else:
-            print('....')
-        # convert random number to a string
-        # dataForm['number'] = str(dataForm['number'])
-        # get matching row from data where the number is equal to the number from the form
-        data = data[data['No'] == dataForm['number']]
-        print('Data selected: ', data.to_dict('records'))
-        return render_template('results.html', data = data)
+        print(f"Data from form: %s", dataForm)
+        
+        if not dataForm:
+            dataForm = {'number': random.randint(1, 1010)}
+            print(f"Random number selected: %s", dataForm['number'])
+            
+        local_data = poke_data.query(f'No == {int(dataForm["number"])}')
+        print(f"Data selected: %s", local_data.to_dict('records'))
+        
+        poke_data = local_data.copy()
+        
+        return render_template('results.html', data=local_data)
+    return render_template('home.html') 
 
-    return render_template('home.html')
-
-@app.route('/results')
+@app.route('/results', methods = ['GET', 'POST'])
 def results():
-    return render_template('results.html')
+    return render_template('results.html', data=poke_data)
+ 
+@app.route('/choices', methods = ['GET', 'POST'])
+def choices():
+    return render_template('choices.html', data = poke_data)
+
 
 app.run(host = '0.0.0.0', port = 8080, debug = True, use_reloader = True)
+
